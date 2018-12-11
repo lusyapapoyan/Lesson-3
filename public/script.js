e5f215082b21633ea90a95641362a688ca773985
Your Name <you@example.com>
2018-12-11 14:39:49 +0400

    modified scrpit.js

--- public/script.js
+++ public/script.js
@@ -58,34 +58,41 @@ function draw() {
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x].index == 1) {
-                fill("green");
-                rect(x * side, y * side, side, side);
+                // fill("green");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
             }
             else if (matrix[y][x] == 0) {
-                fill("#acacac");
-                rect(x * side, y * side, side, side);
+                // fill("#acacac");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
+
             }
             else if (matrix[y][x].index == 2) {
-                fill("yellow");
-                rect(x * side, y * side, side, side);
+                // fill("yellow");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
                 matrix[y][x].acted = false;
 
             }
             else if (matrix[y][x].index == 3) {
-                fill("red");
-                rect(x * side, y * side, side, side);
+                // fill("red");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
                 matrix[y][x].acted = false;
 
             }
             else if (matrix[y][x].index == 4) {
-                fill("#99CCFF");
-                rect(x * side, y * side, side, side);
+                // fill("#99CCFF");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
                 matrix[y][x].acted = false;
 
             }
             else if (matrix[y][x].index == 5) {
-                fill("#000066");
-                rect(x * side, y * side, side, side);
+                // fill("#000066");
+                // rect(x * side, y * side, side, side);
+                console.log(matrix[y][x].index);
                 matrix[y][x].acted = false;
 
             }
